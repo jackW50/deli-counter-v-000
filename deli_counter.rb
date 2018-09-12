88675f2457149792058623d01000e82b950a592a
John Werenski <jtwerenski@gmail.com>
2018-09-12 13:19:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+
+#def line(array)
+#  counter = 0 
+ # new_array = []
+ # array.each do |element|
+ #   new_array << "#{counter + 1}. #{element}"
+#  end 
+#  puts "The line is currently: #{new_array.join(" ")"
+#end 
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  
+  else 
+    counter = 1 
+    new_array = []
+    array.each do |element|
+      new_array << "#{counter}. #{element}"
+      counter += 1 
+    end 
+    puts "The line is currently: #{new_array.join(" ")}"
+  end 
+end 
+
+def take_a_number(array, name)
+  
+  
